edbe431e1f1e3375c5034bc7dc820e8edccd5b42
Dimitriy <dleksanov@stuy.edu>
2018-03-01 14:00:24 -0500

    first commit

--- svg.js
+++ svg.js
@@ -1,39 +1,87 @@
-var drawLine = false;
+var growing = true;
 var pic = document.getElementById("pic");
 var clearButt = document.getElementById("clear");
-var circleR = 10;
-var oldX = 0;
-var oldY = 0;
+var circleButt = document.getElementById("circle");
+var dvdButt = document.getElementById("dvd");
+var circleR = 0;
+var rectX = 150;
+var rectY = 200;
+var vX = 2;
+var vY = 1;
 
-var addCircle = function(e){
+var growId;
+var dvdId;
+
+var growCircle = function(e){
     var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    var l1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
-    c1.setAttribute("cx", e.offsetX);
-    c1.setAttribute("cy", e.offsetY);
+    c1.setAttribute("cx", 150);
+    c1.setAttribute("cy", 200);
     c1.setAttribute("r", circleR);
     pic.appendChild(c1);
-
-    if(drawLine == true){
-	l1.setAttribute("x1", oldX);
-	l1.setAttribute("y1", oldY);
-	l1.setAttribute("x2", e.offsetX);
-	l1.setAttribute("y2", e.offsetY);
-	l1.setAttribute("style", "stroke:rgb(255,0,0);stroke-width:2");
-	pic.appendChild(l1);
+    
+    if(growing == true){
+	circleR += 1;
+	if(circleR >= 150){
+	    growing = false;
+	}
+    }
+    else{
+	circleR -= 1;
+	if(circleR <= 0){
+	    growing = true;
+	}
     }
 
-    drawLine = true;
-    oldX = e.offsetX;
-    oldY = e.offsetY;
+    pic.removeChild(pic.childNodes[0]);
 }
 
-pic.addEventListener("click", addCircle);
+var growWrap = function(e){
+    clearInterval(dvdId);
+    rectX = 150;
+    rectY = 200;
+    growId = setInterval(growCircle, 25);
+}
+
+circleButt.addEventListener("click", growWrap);
+
+var dvd = function(e){
+    var r1 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+
+    r1.setAttribute("x", rectX);
+    r1.setAttribute("y", rectY);
+    r1.setAttribute("width", 80);
+    r1.setAttribute("height", 60);
+    pic.appendChild(r1);
+
+    rectX += vX;
+    rectY += vY;
+
+    if(rectX >= 220 || rectX <= 40){
+	vX *= -1;
+    }
+    if(rectY >= 340 || rectY <= 30){
+	vY *= -1;
+    }
+    
+    pic.removeChild(pic.childNodes[0]);
+}
+
+var dvdWrap = function(e){
+    clearInterval(growId);
+    circleR = 0;
+    dvdId = setInterval(dvd, 25);
+}
+
+dvdButt.addEventListener("click", dvdWrap);
 
 var clearFxn = function(e){
+    
     while(pic.childNodes[0] != null){
 	pic.removeChild(pic.childNodes[0]);
     }
+    clearInterval(growId);
+    clearInterval(dvdId);
 }
 
 clearButt.addEventListener("click", clearFxn);
