e750e83c51990881186296d4f8198e36c2e6caa9
DimitriyLeks <dimitriyl@DimitriLekssMBP.fios-router.home>
2018-02-28 19:13:02 -0500

    final commit

--- svg.js
+++ svg.js
@@ -1,22 +1,40 @@
+var drawLine = false;
 var pic = document.getElementById("pic");
 var clearButt = document.getElementById("clear");
-var circleX = 500;
-var circleY = 400;
-var circleR = 50;
+var circleR = 10;
+var oldX = 0;
+var oldY = 0;
 
 var addCircle = function(e){
     var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c1.setAttribute("x", "120");
-    c1.setAttribute("y", "70");
-    c1.setAttribute("r", "30");
+    var l1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+    c1.setAttribute("cx", e.offsetX);
+    c1.setAttribute("cy", e.offsetY);
+    c1.setAttribute("r", circleR);
     pic.appendChild(c1);
+
+    if(drawLine == true){
+	l1.setAttribute("x1", oldX);
+	l1.setAttribute("y1", oldY);
+	l1.setAttribute("x2", e.offsetX);
+	l1.setAttribute("y2", e.offsetY);
+	l1.setAttribute("style", "stroke:rgb(255,0,0);stroke-width:2");
+	pic.appendChild(l1);
+    }
+
+    drawLine = true;
+    oldX = e.offsetX;
+    oldY = e.offsetY;
 }
 
-clearButt.addEventListener("click", addCircle);
+pic.addEventListener("click", addCircle);
 
-/*var clearFxn = function(e){
-    ctx.clearRect(0, 0, 500, 500);
+var clearFxn = function(e){
+    while(pic.childNodes[0] != null){
+	pic.removeChild(pic.childNodes[0]);
+    }
 }
 
-clearButt.addEventListener("click", clearFxn);*/
+clearButt.addEventListener("click", clearFxn);
 
